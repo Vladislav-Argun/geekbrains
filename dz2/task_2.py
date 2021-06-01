933f9f65869662129540725dd182ff633a98ccb2
╨Ш╨╝╤П ╨┐╨╛╨╗╤М╨╖╨╛╨▓╨░╤В╨╡╨╗╤П <>
2021-06-01 16:36:02 +0300

    New changes

--- dz2/task_2.py
+++ dz2/task_2.py
@@ -6,7 +6,7 @@ while True:
 	print(a)
 	if ('exit' in a):
 		a.pop(-1)
-		for i in range(len(a)):
-		    a[i - 1], a[i] = a[i], a[i - 1]
+		for i in range(len(a)/4):
+		    a[i], a[i + 1] = b [i + 1], a[i]
 		print('    '.join([str(i) for i in a]))
 		break
